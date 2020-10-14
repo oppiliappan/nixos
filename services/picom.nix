021db58b7b10d04afc8c2a705a9725081d58b901
Akshay <nerdy@peppe.rs>
2020-10-14 22:33:34 +0530

    remove inactive dim

--- services/picom.nix
+++ services/picom.nix
@@ -4,7 +4,7 @@
   fade = true;
   fadeDelta = 10;
   fadeSteps = [ "0.04" "0.04" ];
-  inactiveDim = "0.2";
+  inactiveDim = "0.0";
   shadow = false;
   shadowOffsets = [ (-60) (-60) ];
   shadowOpacity = "0.20";
